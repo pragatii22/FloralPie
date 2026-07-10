230cf2120c4df6d8bba99d43988bbefe0a9e944e
Pragati Gaire <pragatishahi022@gmail.com>
2026-07-10 19:29:03 +0545

    Updated project configuration for better workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.compose.runtime.livedata)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
